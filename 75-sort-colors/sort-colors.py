4777e9888cdb4ec73b7512da57efa8285073f27c
Kalid <155190302+Kalx6@users.noreply.github.com>
2025-04-07 16:59:05 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.19%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+       nums.sort() 
+        
+        
+        
+       return nums
+       """
+        Do not return anything, modify nums in-place instead.
+       """
+        
