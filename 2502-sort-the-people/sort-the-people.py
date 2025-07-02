6f1ce917a73f73717fd72fe0ddc080ea22bea4d1
Kalid <155190302+Kalx6@users.noreply.github.com>
2025-07-02 05:10:34 +0000

    Update sort-the-people.py

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,18 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        my_dict={}
-        res=[]
+        import copy
+
+        ret = [0]*len(names)
+
+        copy_height = copy.deepcopy(heights)
+
+        copy_height.sort(reverse=True)
+
         for i in range(len(names)):
-            my_dict[heights[i]]=names[i]
-        
-        heights.sort(reverse=True)
-
-        for i in heights:
-            
-            res.append(my_dict[i])
+            ret[i] = names[heights.index(copy_height[i])]
         
 
-        return res
+        return ret
+
 
       
         
